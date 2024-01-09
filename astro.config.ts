810d27a00547a97db4a87d1c6b78dde219c35aa9
saivishwak <saivishwak40@gmail.com>
2024-01-09 17:58:22 +0530

    remove base

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,6 @@ import { visualizer } from 'rollup-plugin-visualizer';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://saivishwak.com',
-  base: '/portfolio',
   integrations: [tailwind(), image(), compress()],
   vite: {
     plugins: [visualizer()],
